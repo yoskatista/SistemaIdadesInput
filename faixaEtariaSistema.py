7e8a3fa66c866859dfb45821f9e74753774dcc00
Mikael Cassamassimo Borges <66427859+yoskatista@users.noreply.github.com>
2021-02-18 13:15:08 -0300

    FaixaEtariaSisteminhaInput

---
+++ faixaEtariaSistema.py
@@ -0,0 +1,19 @@
+idade = int(input('Qual a sua idade?'))
+
+if idade <= 2:
+    print('Você é um Bebê')
+
+if idade < 5:
+    print('Você ainda é uma criança')
+
+if idade < 13:
+    print('Você ainda é um Garoto(a)')
+
+if idade < 20:
+    print('Você ainda é um adolecente')
+
+if idade < 65:
+    print('Você ainda é um adulto')
+
+if idade > 65:
+    print('Você já é um idoso')
